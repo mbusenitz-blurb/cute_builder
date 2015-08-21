eb955766b0f80f3c82e467f7c42e55b10dac7192
mbusenitz-blurb <mbusenitz@blurb.com>
2015-08-21 16:17:00 -0700

    mac cutie

--- cutie.js
+++ cutie.js
@@ -1,9 +1,12 @@
-var cp = require( 'child_process' )
-  , Agent = require( './agent' )
-  , agent = new Agent( '' );
+var Agent = require( './agent' )
+  , OS = require( './os' )
+  , agent = new Agent( '/Users/mbusenitz/Qt5' )
+  , os = new OS(agent);
 
+agent.after( 'check permissions done', 'check working dir' );
 agent.after( 'check working dir done', 'check env' );
-agent.after( 'check env done', 'configure' );
-agent.after( 'configure done', 'build' );
-agent.after( 'build done', console.log.bind( null, 'done' ) );
-agent.emit( 'check working dir' ); 
+agent.after( 'check env done', os.configure );
+agent.after( 'configure done', os.build );
+agent.after( 'build done', os.install );
+agent.after( 'install done', console.log.bind( null, 'done' ) );
+agent.emit( 'check permissions' ); 
