25ef8a2d9fe421586b012ee452368bfaca80f991
mbusenitz-blurb <mbusenitz@blurb.com>
2015-08-22 07:45:23 -0700

    sudo check

--- check_env.js
+++ check_env.js
@@ -1,5 +1,11 @@
 var cp = require( 'child_process' );
 
+var uid = parseInt( process.env.SUDO_UID );
+if (!uid) {
+	throw( 'cute builder must be run with sudo/admin privileges!' );    
+}  
+process.setuid( uid );
+
 check( 'ruby -v' );
 check( 'python --version' );
 check( 'perl -v' );
