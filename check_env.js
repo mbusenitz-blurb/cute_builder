16cc67b7f3f91777042c2a2ce550f16e5d658666
mbusenitz-blurb <mbusenitz@blurb.com>
2015-08-25 12:46:11 -0700

    check sudo on mac

--- check_env.js
+++ check_env.js
@@ -1,10 +1,12 @@
 var cp = require( 'child_process' );
 
-var uid = parseInt( process.env.SUDO_UID );
-if (!uid) {
-	throw( 'cute builder must be run with sudo/admin privileges!' );    
-}  
-process.setuid( uid );
+if (process.platform !== 'win32') {
+	var uid = parseInt( process.env.SUDO_UID );
+	if (!uid) {
+		throw( 'cute builder must be run with sudo/admin privileges!' );    
+	}  
+	process.setuid( uid );
+}
 
 check( 'ruby -v' );
 check( 'python --version' );
